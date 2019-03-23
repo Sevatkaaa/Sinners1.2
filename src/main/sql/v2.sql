2fae2bb2f9e0d67b6fdde8b268a7863eaa7821b0
Mark Anpilogov <mrharrou@gmail.com>
2019-03-23 13:54:04 +0200

    Fix readme

--- src/main/sql/v2.sql
+++
@@ -1,8 +0,0 @@
-UPDATE `sin_version` SET `version`=2 WHERE `version`=1 LIMIT 1;
-
-ALTER TABLE `chapters`
-  ADD COLUMN `url` text NULL DEFAULT NULL,
-  DROP COLUMN `title`;
-
-ALTER TABLE `verses`
-  ADD COLUMN `verse_id` int(11) NOT NULL AFTER `id`;
