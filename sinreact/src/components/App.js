50a26fa11121546d56e22ce26b164b64e2e18d20
Mark Anpilogov <mrharrou@gmail.com>
2019-03-31 09:03:14 +0300

    Routing

--- sinreact/src/components/App.js
+++ sinreact/src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './App.css'
-import FakeSinDashboard from '../containers/FakeSinDashboard';
+import FakeSinDashboard from '../containers/FakeSinDashboard'
+import Error from './ErrorView'
+import UserLogin from './UserLogin'
+import Header from './Header'
+import {Route, Switch} from 'react-router-dom'
+import BibleView from './BibleView'
+import Profile from './Profile'
+
 
 const App = () => (
-  <FakeSinDashboard/>
+    <div>
+        <Header/>
+        <Switch>
+            <Route exact path="/login" component={UserLogin}/>
+            <Route path="/dashboard" component={FakeSinDashboard}/>
+            <Route path="/bible" component={BibleView}/>
+            <Route path="/profile" component={Profile}/>
+            <Route path="/" component={Error}/>
+        </Switch>
+    </div>
 )
 
 export default App;
--- sinreact/src/index.js
+++ sinreact/src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import {store} from './store/store'
+import './index.css'
+import App from './components/App'
+import UserLogin from './components/UserLogin'
+import FakeSinDashboard from './containers/FakeSinDashboard'
+import * as serviceWorker from './serviceWorker'
+import { store } from './store/store'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core'
+import { Route, Router, BrowserRouter } from 'react-router-dom'
 
-const rootElement = document.getElementById('root')
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#373737'
+        },
+        secondary: {
+            main: '#F5F5F5'
+        },
+    }
+});
+
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	rootElement
+    <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </MuiThemeProvider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
